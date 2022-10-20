925d9d73c6de277932a5064c8e97b899258cf863
amenir88 <ameni.rommene@esprit.tn>
2022-10-20 12:07:18 +0100

    récupérer les informations d'un formulaire

--- 4TWIN2/angularTwin2/src/app/user/user-form/user-form.component.ts
+++ 4TWIN2/angularTwin2/src/app/user/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
+  list : User[]=[];
+  user : User = new User();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addUser(f){
+    console.log(f);
+    this.list.push(this.user);
+    console.log(this.list);
   }
 
 }
